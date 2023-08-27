a3c4f442f78b357432aebd6a508fcf2661091509
ArkamaHozota <arkamahozota@hotmail.com>
2023-08-27 18:04:39 +0900

    1.20

---
+++ src/main/java/jp/co/reggie/jpadeal/repository/UsersRepository.java
@@ -0,0 +1,14 @@
+package jp.co.reggie.jpadeal.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import jp.co.reggie.jpadeal.entity.User;
+
+/**
+ * 客戶信息數據接口
+ *
+ * @author Administrator
+ * @date 2022-11-29
+ */
+public interface UsersRepository extends JpaRepository<User, Long> {
+}
