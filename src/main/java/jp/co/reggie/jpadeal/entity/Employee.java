288aa5adda2525fdc5176b49ec5bdf98fe161358
ArkamaHozota <arkamahozota@hotmail.com>
2023-08-27 17:44:38 +0900

    1.04

--- src/main/java/jp/co/reggie/jpadeal/entity/Employee.java
+++ src/main/java/jp/co/reggie/jpadeal/entity/Employee.java
@@ -2,52 +2,67 @@ package jp.co.reggie.jpadeal.entity;
 
 import java.io.Serializable;
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 /**
  * 員工管理實體類
  *
  * @author Administrator
  */
-@Data
-@EqualsAndHashCode(callSuper = false)
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "employee")
 public class Employee extends BasicEntity implements Serializable {
 
 	private static final long serialVersionUID = -6540113185665801143L;
 
 	/**
-	 * 賬號名
-	 */
-	private String username;
-
-	/**
 	 * 姓名
 	 */
+	@Column(nullable = false)
 	private String name;
+
+	/**
+	 * 賬號名
+	 */
+	@Column(nullable = false)
+	private String username;
 
 	/**
 	 * 密碼
 	 */
+	@Column(nullable = false)
 	private String password;
 
 	/**
 	 * 手機號
 	 */
+	@Column(name = "phone_num", nullable = false)
 	private String phoneNo;
 
 	/**
 	 * 性別
 	 */
+	@Column(name = "sex", nullable = false)
 	private String gender;
 
 	/**
 	 * 身份證號
 	 */
+	@Column(nullable = false)
 	private String idNumber;
 
 	/**
 	 * 賬號狀態：0:禁用，1:正常
 	 */
+	@Column(nullable = false)
 	private Integer status;
 }
