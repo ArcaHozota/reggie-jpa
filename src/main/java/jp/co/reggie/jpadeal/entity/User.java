5ff729a3c776c4d39c9124b989e94acb3043e068
ArkamaHozota <arkamahozota@hotmail.com>
2023-08-27 17:53:51 +0900

    1.09

--- src/main/java/jp/co/reggie/jpadeal/entity/User.java
+++ src/main/java/jp/co/reggie/jpadeal/entity/User.java
@@ -2,14 +2,25 @@ package jp.co.reggie.jpadeal.entity;
 
 import java.io.Serializable;
 
-import lombok.Data;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 /**
  * 客戶信息實體類
  *
  * @author Administrator
  */
-@Data
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "user")
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 2324630650798877027L;
@@ -17,6 +28,7 @@ public class User implements Serializable {
 	/**
 	 * ID
 	 */
+	@Id
 	private Long id;
 
 	/**
@@ -27,11 +39,13 @@ public class User implements Serializable {
 	/**
 	 * 手機號
 	 */
+	@Column(name = "phone_num", nullable = false)
 	private String phoneNo;
 
 	/**
 	 * 性別：0女，1男
 	 */
+	@Column(name = "sex")
 	private String gender;
 
 	/**
