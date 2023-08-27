22c4b751e58808ae9c99d10558349cd00aa316a1
ArkamaHozota <arkamahozota@hotmail.com>
2023-08-27 17:38:22 +0900

    1.02

--- src/main/java/jp/co/reggie/jpadeal/entity/Category.java
+++ src/main/java/jp/co/reggie/jpadeal/entity/Category.java
@@ -2,16 +2,24 @@ package jp.co.reggie.jpadeal.entity;
 
 import java.io.Serializable;
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 /**
  * 分類管理實體類
  *
  * @author Administrator
  */
-@Data
-@EqualsAndHashCode(callSuper = false)
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "category")
 public class Category extends BasicEntity implements Serializable {
 
 	private static final long serialVersionUID = -5583580956537498025L;
@@ -24,15 +32,18 @@ public class Category extends BasicEntity implements Serializable {
 	/**
 	 * 分類名稱
 	 */
+	@Column(nullable = false)
 	private String name;
 
 	/**
 	 * 順序
 	 */
+	@Column(nullable = false)
 	private Integer sort;
 
 	/**
 	 * 邏輯刪除字段
 	 */
+	@Column(nullable = false)
 	private String logicDeleteFlg;
 }
