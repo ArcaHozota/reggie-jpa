a4b015c235283dcb4323fd868978bc4ae50b83b0
Arca Hozota <arkamahozota@gmail.com>
2023-09-01 16:45:50 +0900

    1.67

--- src/main/java/jp/co/reggie/jpadeal/repository/ShoppingCartRepository.java
+++ src/main/java/jp/co/reggie/jpadeal/repository/ShoppingCartRepository.java
@@ -2,15 +2,17 @@ package jp.co.reggie.jpadeal.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 
 import jp.co.reggie.jpadeal.entity.ShoppingCart;
 
 /**
- * 購物車數據接口
+ * ショッピングカートリポジトリ
  *
  * @author Administrator
  * @date 2022-11-29
  */
+@Repository
 public interface ShoppingCartRepository
 		extends JpaRepository<ShoppingCart, Long>, JpaSpecificationExecutor<ShoppingCart> {
 }
--- src/main/java/jp/co/reggie/jpadeal/repository/UsersRepository.java
+++ src/main/java/jp/co/reggie/jpadeal/repository/UsersRepository.java
@@ -2,14 +2,16 @@ package jp.co.reggie.jpadeal.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 
 import jp.co.reggie.jpadeal.entity.User;
 
 /**
- * 客戶信息數據接口
+ * ユーザ情報リポジトリ
  *
  * @author Administrator
  * @date 2022-11-29
  */
+@Repository
 public interface UsersRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
 }
