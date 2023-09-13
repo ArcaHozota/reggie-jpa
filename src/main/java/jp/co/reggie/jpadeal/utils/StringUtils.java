8b4e2fafc27cc2832a8750190342e7a5436d9a8a
Arca Hozota <arkamahozota@gmail.com>
2023-09-13 14:17:34 +0900

    2.60

--- src/main/java/jp/co/reggie/jpadeal/utils/StringUtils.java
+++ src/main/java/jp/co/reggie/jpadeal/utils/StringUtils.java
@@ -34,7 +34,7 @@ public final class StringUtils {
 	/**
 	 * 全角半角変換マップ
 	 */
-	private static final BidiMap<String, String> HALF_FULL_CONVERTAR = new DualHashBidiMap<>();
+	private static final BidiMap<String, String> HALF_FULL_CONVERTOR = new DualHashBidiMap<>();
 
 	/**
 	 * 該当文字列はすべて半角かどうかを判断する
@@ -42,8 +42,8 @@ public final class StringUtils {
 	 * @param hankakuString 文字列
 	 * @return true: すべて半角文字列, false: 全角文字も含める
 	 */
-	public static boolean isHankaku(final String hankakuString) {
-		final List<String> zenkakuList = new ArrayList<>(HALF_FULL_CONVERTAR.keySet());
+	public static boolean isAllHankaku(final String hankakuString) {
+		final List<String> zenkakuList = new ArrayList<>(HALF_FULL_CONVERTOR.keySet());
 		for (final char aChar : hankakuString.toCharArray()) {
 			if (zenkakuList.contains(String.valueOf(aChar))) {
 				return false;
@@ -55,11 +55,11 @@ public final class StringUtils {
 	/**
 	 * 該当文字列はすべて全角かどうかを判断する
 	 *
-	 * @param hankakuString 文字列
+	 * @param zenkakuString 文字列
 	 * @return true: すべて全角文字列, false: 半角文字も含める
 	 */
-	public static boolean isZenkaku(final String zenkakuString) {
-		final List<String> hankakuList = new ArrayList<>(HALF_FULL_CONVERTAR.values());
+	public static boolean isAllZenkaku(final String zenkakuString) {
+		final List<String> hankakuList = new ArrayList<>(HALF_FULL_CONVERTOR.values());
 		for (final char aChar : zenkakuString.toCharArray()) {
 			if (hankakuList.contains(String.valueOf(aChar))) {
 				return false;
@@ -79,11 +79,11 @@ public final class StringUtils {
 			return EMPTY_STRING;
 		}
 		final StringBuilder builder = new StringBuilder();
-		final List<String> zenkakuList = new ArrayList<>(HALF_FULL_CONVERTAR.keySet());
+		final List<String> zenkakuList = new ArrayList<>(HALF_FULL_CONVERTOR.keySet());
 		for (int i = 0; i < zenkaku.length(); i++) {
 			final String charAtString = String.valueOf(zenkaku.charAt(i));
 			if (zenkakuList.contains(charAtString)) {
-				builder.append(HALF_FULL_CONVERTAR.get(charAtString));
+				builder.append(HALF_FULL_CONVERTOR.get(charAtString));
 			} else {
 				builder.append(charAtString);
 			}
@@ -102,11 +102,11 @@ public final class StringUtils {
 			return EMPTY_STRING;
 		}
 		final StringBuilder builder = new StringBuilder();
-		final List<String> hankakuList = new ArrayList<>(HALF_FULL_CONVERTAR.values());
+		final List<String> hankakuList = new ArrayList<>(HALF_FULL_CONVERTOR.values());
 		for (int i = 0; i < hankaku.length(); i++) {
 			final String charAtString = String.valueOf(hankaku.charAt(i));
 			if (hankakuList.contains(charAtString)) {
-				builder.append(HALF_FULL_CONVERTAR.inverseBidiMap().get(charAtString));
+				builder.append(HALF_FULL_CONVERTOR.inverseBidiMap().get(charAtString));
 			} else {
 				builder.append(charAtString);
 			}
@@ -121,7 +121,7 @@ public final class StringUtils {
 	 * @return true: 空, false: 空ではない
 	 */
 	public static boolean isEmpty(@Nullable final String str) {
-		return str == null || str.length() == 0 || str.isBlank();
+		return str == null || str.isEmpty() || str.isBlank();
 	}
 
 	/**
@@ -165,191 +165,191 @@ public final class StringUtils {
 	}
 
 	static {
-		HALF_FULL_CONVERTAR.put("！", "!");
-		HALF_FULL_CONVERTAR.put("”", "\"");
-		HALF_FULL_CONVERTAR.put("＃", "#");
-		HALF_FULL_CONVERTAR.put("＄", "$");
-		HALF_FULL_CONVERTAR.put("％", "%");
-		HALF_FULL_CONVERTAR.put("＆", "&");
-		HALF_FULL_CONVERTAR.put("’", "'");
-		HALF_FULL_CONVERTAR.put("（", "(");
-		HALF_FULL_CONVERTAR.put("）", ")");
-		HALF_FULL_CONVERTAR.put("＊", "*");
-		HALF_FULL_CONVERTAR.put("＋", "+");
-		HALF_FULL_CONVERTAR.put("，", ",");
-		HALF_FULL_CONVERTAR.put("－", "-");
-		HALF_FULL_CONVERTAR.put("．", ".");
-		HALF_FULL_CONVERTAR.put("／", "/");
-		HALF_FULL_CONVERTAR.put("０", "0");
-		HALF_FULL_CONVERTAR.put("１", "1");
-		HALF_FULL_CONVERTAR.put("２", "2");
-		HALF_FULL_CONVERTAR.put("３", "3");
-		HALF_FULL_CONVERTAR.put("４", "4");
-		HALF_FULL_CONVERTAR.put("５", "5");
-		HALF_FULL_CONVERTAR.put("６", "6");
-		HALF_FULL_CONVERTAR.put("７", "7");
-		HALF_FULL_CONVERTAR.put("８", "8");
-		HALF_FULL_CONVERTAR.put("９", "9");
-		HALF_FULL_CONVERTAR.put("：", ":");
-		HALF_FULL_CONVERTAR.put("；", ";");
-		HALF_FULL_CONVERTAR.put("＜", "<");
-		HALF_FULL_CONVERTAR.put("＝", "=");
-		HALF_FULL_CONVERTAR.put("＞", ">");
-		HALF_FULL_CONVERTAR.put("？", "?");
-		HALF_FULL_CONVERTAR.put("＠", "@");
-		HALF_FULL_CONVERTAR.put("Ａ", "A");
-		HALF_FULL_CONVERTAR.put("Ｂ", "B");
-		HALF_FULL_CONVERTAR.put("Ｃ", "C");
-		HALF_FULL_CONVERTAR.put("Ｄ", "D");
-		HALF_FULL_CONVERTAR.put("Ｅ", "E");
-		HALF_FULL_CONVERTAR.put("Ｆ", "F");
-		HALF_FULL_CONVERTAR.put("Ｇ", "G");
-		HALF_FULL_CONVERTAR.put("Ｈ", "H");
-		HALF_FULL_CONVERTAR.put("Ｉ", "I");
-		HALF_FULL_CONVERTAR.put("Ｊ", "J");
-		HALF_FULL_CONVERTAR.put("Ｋ", "K");
-		HALF_FULL_CONVERTAR.put("Ｌ", "L");
-		HALF_FULL_CONVERTAR.put("Ｍ", "M");
-		HALF_FULL_CONVERTAR.put("Ｎ", "N");
-		HALF_FULL_CONVERTAR.put("Ｏ", "O");
-		HALF_FULL_CONVERTAR.put("Ｐ", "P");
-		HALF_FULL_CONVERTAR.put("Ｑ", "Q");
-		HALF_FULL_CONVERTAR.put("Ｒ", "R");
-		HALF_FULL_CONVERTAR.put("Ｓ", "S");
-		HALF_FULL_CONVERTAR.put("Ｔ", "T");
-		HALF_FULL_CONVERTAR.put("Ｕ", "U");
-		HALF_FULL_CONVERTAR.put("Ｖ", "V");
-		HALF_FULL_CONVERTAR.put("Ｗ", "W");
-		HALF_FULL_CONVERTAR.put("Ｘ", "X");
-		HALF_FULL_CONVERTAR.put("Ｙ", "Y");
-		HALF_FULL_CONVERTAR.put("Ｚ", "Z");
-		HALF_FULL_CONVERTAR.put("［", "[");
-		HALF_FULL_CONVERTAR.put("￥", "\\");
-		HALF_FULL_CONVERTAR.put("］", "]");
-		HALF_FULL_CONVERTAR.put("＾", "^");
-		HALF_FULL_CONVERTAR.put("＿", "_");
-		HALF_FULL_CONVERTAR.put("｀", "`");
-		HALF_FULL_CONVERTAR.put("ａ", "a");
-		HALF_FULL_CONVERTAR.put("ｂ", "b");
-		HALF_FULL_CONVERTAR.put("ｃ", "c");
-		HALF_FULL_CONVERTAR.put("ｄ", "d");
-		HALF_FULL_CONVERTAR.put("ｅ", "e");
-		HALF_FULL_CONVERTAR.put("ｆ", "f");
-		HALF_FULL_CONVERTAR.put("ｇ", "g");
-		HALF_FULL_CONVERTAR.put("ｈ", "h");
-		HALF_FULL_CONVERTAR.put("ｉ", "i");
-		HALF_FULL_CONVERTAR.put("ｊ", "j");
-		HALF_FULL_CONVERTAR.put("ｋ", "k");
-		HALF_FULL_CONVERTAR.put("ｌ", "l");
-		HALF_FULL_CONVERTAR.put("ｍ", "m");
-		HALF_FULL_CONVERTAR.put("ｎ", "n");
-		HALF_FULL_CONVERTAR.put("ｏ", "o");
-		HALF_FULL_CONVERTAR.put("ｐ", "p");
-		HALF_FULL_CONVERTAR.put("ｑ", "q");
-		HALF_FULL_CONVERTAR.put("ｒ", "r");
-		HALF_FULL_CONVERTAR.put("ｓ", "s");
-		HALF_FULL_CONVERTAR.put("ｔ", "t");
-		HALF_FULL_CONVERTAR.put("ｕ", "u");
-		HALF_FULL_CONVERTAR.put("ｖ", "v");
-		HALF_FULL_CONVERTAR.put("ｗ", "w");
-		HALF_FULL_CONVERTAR.put("ｘ", "x");
-		HALF_FULL_CONVERTAR.put("ｙ", "y");
-		HALF_FULL_CONVERTAR.put("ｚ", "z");
-		HALF_FULL_CONVERTAR.put("｛", "{");
-		HALF_FULL_CONVERTAR.put("｜", "|");
-		HALF_FULL_CONVERTAR.put("｝", "}");
-		HALF_FULL_CONVERTAR.put("\uff5e", "~");
-		HALF_FULL_CONVERTAR.put("。", "｡");
-		HALF_FULL_CONVERTAR.put("「", "｢");
-		HALF_FULL_CONVERTAR.put("」", "｣");
-		HALF_FULL_CONVERTAR.put("、", "､");
-		HALF_FULL_CONVERTAR.put("・", "･");
-		HALF_FULL_CONVERTAR.put("ァ", "ｧ");
-		HALF_FULL_CONVERTAR.put("ィ", "ｨ");
-		HALF_FULL_CONVERTAR.put("ゥ", "ｩ");
-		HALF_FULL_CONVERTAR.put("ェ", "ｪ");
-		HALF_FULL_CONVERTAR.put("ォ", "ｫ");
-		HALF_FULL_CONVERTAR.put("ャ", "ｬ");
-		HALF_FULL_CONVERTAR.put("ュ", "ｭ");
-		HALF_FULL_CONVERTAR.put("ョ", "ｮ");
-		HALF_FULL_CONVERTAR.put("ッ", "ｯ");
-		HALF_FULL_CONVERTAR.put("ー", "ｰ");
-		HALF_FULL_CONVERTAR.put("ア", "ｱ");
-		HALF_FULL_CONVERTAR.put("イ", "ｲ");
-		HALF_FULL_CONVERTAR.put("ウ", "ｳ");
-		HALF_FULL_CONVERTAR.put("エ", "ｴ");
-		HALF_FULL_CONVERTAR.put("オ", "ｵ");
-		HALF_FULL_CONVERTAR.put("カ", "ｶ");
-		HALF_FULL_CONVERTAR.put("キ", "ｷ");
-		HALF_FULL_CONVERTAR.put("ク", "ｸ");
-		HALF_FULL_CONVERTAR.put("ケ", "ｹ");
-		HALF_FULL_CONVERTAR.put("コ", "ｺ");
-		HALF_FULL_CONVERTAR.put("サ", "ｻ");
-		HALF_FULL_CONVERTAR.put("シ", "ｼ");
-		HALF_FULL_CONVERTAR.put("ス", "ｽ");
-		HALF_FULL_CONVERTAR.put("セ", "ｾ");
-		HALF_FULL_CONVERTAR.put("ソ", "ｿ");
-		HALF_FULL_CONVERTAR.put("タ", "ﾀ");
-		HALF_FULL_CONVERTAR.put("チ", "ﾁ");
-		HALF_FULL_CONVERTAR.put("ツ", "ﾂ");
-		HALF_FULL_CONVERTAR.put("テ", "ﾃ");
-		HALF_FULL_CONVERTAR.put("ト", "ﾄ");
-		HALF_FULL_CONVERTAR.put("ナ", "ﾅ");
-		HALF_FULL_CONVERTAR.put("ニ", "ﾆ");
-		HALF_FULL_CONVERTAR.put("ヌ", "ﾇ");
-		HALF_FULL_CONVERTAR.put("ネ", "ﾈ");
-		HALF_FULL_CONVERTAR.put("ノ", "ﾉ");
-		HALF_FULL_CONVERTAR.put("ハ", "ﾊ");
-		HALF_FULL_CONVERTAR.put("ヒ", "ﾋ");
-		HALF_FULL_CONVERTAR.put("フ", "ﾌ");
-		HALF_FULL_CONVERTAR.put("ヘ", "ﾍ");
-		HALF_FULL_CONVERTAR.put("ホ", "ﾎ");
-		HALF_FULL_CONVERTAR.put("マ", "ﾏ");
-		HALF_FULL_CONVERTAR.put("ミ", "ﾐ");
-		HALF_FULL_CONVERTAR.put("ム", "ﾑ");
-		HALF_FULL_CONVERTAR.put("メ", "ﾒ");
-		HALF_FULL_CONVERTAR.put("モ", "ﾓ");
-		HALF_FULL_CONVERTAR.put("ヤ", "ﾔ");
-		HALF_FULL_CONVERTAR.put("ユ", "ﾕ");
-		HALF_FULL_CONVERTAR.put("ヨ", "ﾖ");
-		HALF_FULL_CONVERTAR.put("ラ", "ﾗ");
-		HALF_FULL_CONVERTAR.put("リ", "ﾘ");
-		HALF_FULL_CONVERTAR.put("ル", "ﾙ");
-		HALF_FULL_CONVERTAR.put("レ", "ﾚ");
-		HALF_FULL_CONVERTAR.put("ロ", "ﾛ");
-		HALF_FULL_CONVERTAR.put("ワ", "ﾜ");
-		HALF_FULL_CONVERTAR.put("ヲ", "ｦ");
-		HALF_FULL_CONVERTAR.put("ン", "ﾝ");
-		HALF_FULL_CONVERTAR.put("ガ", "ｶﾞ");
-		HALF_FULL_CONVERTAR.put("ギ", "ｷﾞ");
-		HALF_FULL_CONVERTAR.put("グ", "ｸﾞ");
-		HALF_FULL_CONVERTAR.put("ゲ", "ｹﾞ");
-		HALF_FULL_CONVERTAR.put("ゴ", "ｺﾞ");
-		HALF_FULL_CONVERTAR.put("ザ", "ｻﾞ");
-		HALF_FULL_CONVERTAR.put("ジ", "ｼﾞ");
-		HALF_FULL_CONVERTAR.put("ズ", "ｽﾞ");
-		HALF_FULL_CONVERTAR.put("ゼ", "ｾﾞ");
-		HALF_FULL_CONVERTAR.put("ゾ", "ｿﾞ");
-		HALF_FULL_CONVERTAR.put("ダ", "ﾀﾞ");
-		HALF_FULL_CONVERTAR.put("ヂ", "ﾁﾞ");
-		HALF_FULL_CONVERTAR.put("ヅ", "ﾂﾞ");
-		HALF_FULL_CONVERTAR.put("デ", "ﾃﾞ");
-		HALF_FULL_CONVERTAR.put("ド", "ﾄﾞ");
-		HALF_FULL_CONVERTAR.put("バ", "ﾊﾞ");
-		HALF_FULL_CONVERTAR.put("ビ", "ﾋﾞ");
-		HALF_FULL_CONVERTAR.put("ブ", "ﾌﾞ");
-		HALF_FULL_CONVERTAR.put("ベ", "ﾍﾞ");
-		HALF_FULL_CONVERTAR.put("ボ", "ﾎﾞ");
-		HALF_FULL_CONVERTAR.put("パ", "ﾊﾟ");
-		HALF_FULL_CONVERTAR.put("ピ", "ﾋﾟ");
-		HALF_FULL_CONVERTAR.put("プ", "ﾌﾟ");
-		HALF_FULL_CONVERTAR.put("ペ", "ﾍﾟ");
-		HALF_FULL_CONVERTAR.put("ポ", "ﾎﾟ");
-		HALF_FULL_CONVERTAR.put("ヴ", "ｳﾞ");
-		HALF_FULL_CONVERTAR.put("\u30f7", "ﾜﾞ");
-		HALF_FULL_CONVERTAR.put("\u30fa", "ｦﾞ");
-		HALF_FULL_CONVERTAR.put("゛", "ﾞ");
-		HALF_FULL_CONVERTAR.put("゜", "ﾟ");
-		HALF_FULL_CONVERTAR.put("\u3000", " ");
+		HALF_FULL_CONVERTOR.put("！", "!");
+		HALF_FULL_CONVERTOR.put("”", "\"");
+		HALF_FULL_CONVERTOR.put("＃", "#");
+		HALF_FULL_CONVERTOR.put("＄", "$");
+		HALF_FULL_CONVERTOR.put("％", "%");
+		HALF_FULL_CONVERTOR.put("＆", "&");
+		HALF_FULL_CONVERTOR.put("’", "'");
+		HALF_FULL_CONVERTOR.put("（", "(");
+		HALF_FULL_CONVERTOR.put("）", ")");
+		HALF_FULL_CONVERTOR.put("＊", "*");
+		HALF_FULL_CONVERTOR.put("＋", "+");
+		HALF_FULL_CONVERTOR.put("，", ",");
+		HALF_FULL_CONVERTOR.put("－", "-");
+		HALF_FULL_CONVERTOR.put("．", ".");
+		HALF_FULL_CONVERTOR.put("／", "/");
+		HALF_FULL_CONVERTOR.put("０", "0");
+		HALF_FULL_CONVERTOR.put("１", "1");
+		HALF_FULL_CONVERTOR.put("２", "2");
+		HALF_FULL_CONVERTOR.put("３", "3");
+		HALF_FULL_CONVERTOR.put("４", "4");
+		HALF_FULL_CONVERTOR.put("５", "5");
+		HALF_FULL_CONVERTOR.put("６", "6");
+		HALF_FULL_CONVERTOR.put("７", "7");
+		HALF_FULL_CONVERTOR.put("８", "8");
+		HALF_FULL_CONVERTOR.put("９", "9");
+		HALF_FULL_CONVERTOR.put("：", ":");
+		HALF_FULL_CONVERTOR.put("；", ";");
+		HALF_FULL_CONVERTOR.put("＜", "<");
+		HALF_FULL_CONVERTOR.put("＝", "=");
+		HALF_FULL_CONVERTOR.put("＞", ">");
+		HALF_FULL_CONVERTOR.put("？", "?");
+		HALF_FULL_CONVERTOR.put("＠", "@");
+		HALF_FULL_CONVERTOR.put("Ａ", "A");
+		HALF_FULL_CONVERTOR.put("Ｂ", "B");
+		HALF_FULL_CONVERTOR.put("Ｃ", "C");
+		HALF_FULL_CONVERTOR.put("Ｄ", "D");
+		HALF_FULL_CONVERTOR.put("Ｅ", "E");
+		HALF_FULL_CONVERTOR.put("Ｆ", "F");
+		HALF_FULL_CONVERTOR.put("Ｇ", "G");
+		HALF_FULL_CONVERTOR.put("Ｈ", "H");
+		HALF_FULL_CONVERTOR.put("Ｉ", "I");
+		HALF_FULL_CONVERTOR.put("Ｊ", "J");
+		HALF_FULL_CONVERTOR.put("Ｋ", "K");
+		HALF_FULL_CONVERTOR.put("Ｌ", "L");
+		HALF_FULL_CONVERTOR.put("Ｍ", "M");
+		HALF_FULL_CONVERTOR.put("Ｎ", "N");
+		HALF_FULL_CONVERTOR.put("Ｏ", "O");
+		HALF_FULL_CONVERTOR.put("Ｐ", "P");
+		HALF_FULL_CONVERTOR.put("Ｑ", "Q");
+		HALF_FULL_CONVERTOR.put("Ｒ", "R");
+		HALF_FULL_CONVERTOR.put("Ｓ", "S");
+		HALF_FULL_CONVERTOR.put("Ｔ", "T");
+		HALF_FULL_CONVERTOR.put("Ｕ", "U");
+		HALF_FULL_CONVERTOR.put("Ｖ", "V");
+		HALF_FULL_CONVERTOR.put("Ｗ", "W");
+		HALF_FULL_CONVERTOR.put("Ｘ", "X");
+		HALF_FULL_CONVERTOR.put("Ｙ", "Y");
+		HALF_FULL_CONVERTOR.put("Ｚ", "Z");
+		HALF_FULL_CONVERTOR.put("［", "[");
+		HALF_FULL_CONVERTOR.put("￥", "\\");
+		HALF_FULL_CONVERTOR.put("］", "]");
+		HALF_FULL_CONVERTOR.put("＾", "^");
+		HALF_FULL_CONVERTOR.put("＿", "_");
+		HALF_FULL_CONVERTOR.put("｀", "`");
+		HALF_FULL_CONVERTOR.put("ａ", "a");
+		HALF_FULL_CONVERTOR.put("ｂ", "b");
+		HALF_FULL_CONVERTOR.put("ｃ", "c");
+		HALF_FULL_CONVERTOR.put("ｄ", "d");
+		HALF_FULL_CONVERTOR.put("ｅ", "e");
+		HALF_FULL_CONVERTOR.put("ｆ", "f");
+		HALF_FULL_CONVERTOR.put("ｇ", "g");
+		HALF_FULL_CONVERTOR.put("ｈ", "h");
+		HALF_FULL_CONVERTOR.put("ｉ", "i");
+		HALF_FULL_CONVERTOR.put("ｊ", "j");
+		HALF_FULL_CONVERTOR.put("ｋ", "k");
+		HALF_FULL_CONVERTOR.put("ｌ", "l");
+		HALF_FULL_CONVERTOR.put("ｍ", "m");
+		HALF_FULL_CONVERTOR.put("ｎ", "n");
+		HALF_FULL_CONVERTOR.put("ｏ", "o");
+		HALF_FULL_CONVERTOR.put("ｐ", "p");
+		HALF_FULL_CONVERTOR.put("ｑ", "q");
+		HALF_FULL_CONVERTOR.put("ｒ", "r");
+		HALF_FULL_CONVERTOR.put("ｓ", "s");
+		HALF_FULL_CONVERTOR.put("ｔ", "t");
+		HALF_FULL_CONVERTOR.put("ｕ", "u");
+		HALF_FULL_CONVERTOR.put("ｖ", "v");
+		HALF_FULL_CONVERTOR.put("ｗ", "w");
+		HALF_FULL_CONVERTOR.put("ｘ", "x");
+		HALF_FULL_CONVERTOR.put("ｙ", "y");
+		HALF_FULL_CONVERTOR.put("ｚ", "z");
+		HALF_FULL_CONVERTOR.put("｛", "{");
+		HALF_FULL_CONVERTOR.put("｜", "|");
+		HALF_FULL_CONVERTOR.put("｝", "}");
+		HALF_FULL_CONVERTOR.put("～", "~");
+		HALF_FULL_CONVERTOR.put("。", "｡");
+		HALF_FULL_CONVERTOR.put("「", "｢");
+		HALF_FULL_CONVERTOR.put("」", "｣");
+		HALF_FULL_CONVERTOR.put("、", "､");
+		HALF_FULL_CONVERTOR.put("・", "･");
+		HALF_FULL_CONVERTOR.put("ァ", "ｧ");
+		HALF_FULL_CONVERTOR.put("ィ", "ｨ");
+		HALF_FULL_CONVERTOR.put("ゥ", "ｩ");
+		HALF_FULL_CONVERTOR.put("ェ", "ｪ");
+		HALF_FULL_CONVERTOR.put("ォ", "ｫ");
+		HALF_FULL_CONVERTOR.put("ャ", "ｬ");
+		HALF_FULL_CONVERTOR.put("ュ", "ｭ");
+		HALF_FULL_CONVERTOR.put("ョ", "ｮ");
+		HALF_FULL_CONVERTOR.put("ッ", "ｯ");
+		HALF_FULL_CONVERTOR.put("ー", "ｰ");
+		HALF_FULL_CONVERTOR.put("ア", "ｱ");
+		HALF_FULL_CONVERTOR.put("イ", "ｲ");
+		HALF_FULL_CONVERTOR.put("ウ", "ｳ");
+		HALF_FULL_CONVERTOR.put("エ", "ｴ");
+		HALF_FULL_CONVERTOR.put("オ", "ｵ");
+		HALF_FULL_CONVERTOR.put("カ", "ｶ");
+		HALF_FULL_CONVERTOR.put("キ", "ｷ");
+		HALF_FULL_CONVERTOR.put("ク", "ｸ");
+		HALF_FULL_CONVERTOR.put("ケ", "ｹ");
+		HALF_FULL_CONVERTOR.put("コ", "ｺ");
+		HALF_FULL_CONVERTOR.put("サ", "ｻ");
+		HALF_FULL_CONVERTOR.put("シ", "ｼ");
+		HALF_FULL_CONVERTOR.put("ス", "ｽ");
+		HALF_FULL_CONVERTOR.put("セ", "ｾ");
+		HALF_FULL_CONVERTOR.put("ソ", "ｿ");
+		HALF_FULL_CONVERTOR.put("タ", "ﾀ");
+		HALF_FULL_CONVERTOR.put("チ", "ﾁ");
+		HALF_FULL_CONVERTOR.put("ツ", "ﾂ");
+		HALF_FULL_CONVERTOR.put("テ", "ﾃ");
+		HALF_FULL_CONVERTOR.put("ト", "ﾄ");
+		HALF_FULL_CONVERTOR.put("ナ", "ﾅ");
+		HALF_FULL_CONVERTOR.put("ニ", "ﾆ");
+		HALF_FULL_CONVERTOR.put("ヌ", "ﾇ");
+		HALF_FULL_CONVERTOR.put("ネ", "ﾈ");
+		HALF_FULL_CONVERTOR.put("ノ", "ﾉ");
+		HALF_FULL_CONVERTOR.put("ハ", "ﾊ");
+		HALF_FULL_CONVERTOR.put("ヒ", "ﾋ");
+		HALF_FULL_CONVERTOR.put("フ", "ﾌ");
+		HALF_FULL_CONVERTOR.put("ヘ", "ﾍ");
+		HALF_FULL_CONVERTOR.put("ホ", "ﾎ");
+		HALF_FULL_CONVERTOR.put("マ", "ﾏ");
+		HALF_FULL_CONVERTOR.put("ミ", "ﾐ");
+		HALF_FULL_CONVERTOR.put("ム", "ﾑ");
+		HALF_FULL_CONVERTOR.put("メ", "ﾒ");
+		HALF_FULL_CONVERTOR.put("モ", "ﾓ");
+		HALF_FULL_CONVERTOR.put("ヤ", "ﾔ");
+		HALF_FULL_CONVERTOR.put("ユ", "ﾕ");
+		HALF_FULL_CONVERTOR.put("ヨ", "ﾖ");
+		HALF_FULL_CONVERTOR.put("ラ", "ﾗ");
+		HALF_FULL_CONVERTOR.put("リ", "ﾘ");
+		HALF_FULL_CONVERTOR.put("ル", "ﾙ");
+		HALF_FULL_CONVERTOR.put("レ", "ﾚ");
+		HALF_FULL_CONVERTOR.put("ロ", "ﾛ");
+		HALF_FULL_CONVERTOR.put("ワ", "ﾜ");
+		HALF_FULL_CONVERTOR.put("ヲ", "ｦ");
+		HALF_FULL_CONVERTOR.put("ン", "ﾝ");
+		HALF_FULL_CONVERTOR.put("ガ", "ｶﾞ");
+		HALF_FULL_CONVERTOR.put("ギ", "ｷﾞ");
+		HALF_FULL_CONVERTOR.put("グ", "ｸﾞ");
+		HALF_FULL_CONVERTOR.put("ゲ", "ｹﾞ");
+		HALF_FULL_CONVERTOR.put("ゴ", "ｺﾞ");
+		HALF_FULL_CONVERTOR.put("ザ", "ｻﾞ");
+		HALF_FULL_CONVERTOR.put("ジ", "ｼﾞ");
+		HALF_FULL_CONVERTOR.put("ズ", "ｽﾞ");
+		HALF_FULL_CONVERTOR.put("ゼ", "ｾﾞ");
+		HALF_FULL_CONVERTOR.put("ゾ", "ｿﾞ");
+		HALF_FULL_CONVERTOR.put("ダ", "ﾀﾞ");
+		HALF_FULL_CONVERTOR.put("ヂ", "ﾁﾞ");
+		HALF_FULL_CONVERTOR.put("ヅ", "ﾂﾞ");
+		HALF_FULL_CONVERTOR.put("デ", "ﾃﾞ");
+		HALF_FULL_CONVERTOR.put("ド", "ﾄﾞ");
+		HALF_FULL_CONVERTOR.put("バ", "ﾊﾞ");
+		HALF_FULL_CONVERTOR.put("ビ", "ﾋﾞ");
+		HALF_FULL_CONVERTOR.put("ブ", "ﾌﾞ");
+		HALF_FULL_CONVERTOR.put("ベ", "ﾍﾞ");
+		HALF_FULL_CONVERTOR.put("ボ", "ﾎﾞ");
+		HALF_FULL_CONVERTOR.put("パ", "ﾊﾟ");
+		HALF_FULL_CONVERTOR.put("ピ", "ﾋﾟ");
+		HALF_FULL_CONVERTOR.put("プ", "ﾌﾟ");
+		HALF_FULL_CONVERTOR.put("ペ", "ﾍﾟ");
+		HALF_FULL_CONVERTOR.put("ポ", "ﾎﾟ");
+		HALF_FULL_CONVERTOR.put("ヴ", "ｳﾞ");
+		HALF_FULL_CONVERTOR.put("ヷ", "ﾜﾞ");
+		HALF_FULL_CONVERTOR.put("ヺ", "ｦﾞ");
+		HALF_FULL_CONVERTOR.put("゛", "ﾞ");
+		HALF_FULL_CONVERTOR.put("゜", "ﾟ");
+		HALF_FULL_CONVERTOR.put("\u3000", " ");
 	}
 }
